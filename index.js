29e769f00defad457b5fd1087032aee5ee04c8e5
JakeVanCleave <jake@jake>
2023-01-16 22:07:20 -0800

    commit tree 0f521f93e9f2d07df4cf986996841785713e8af3

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = [];
+let celebration = "";
+const thankYou = [];
+
+function writeCards(names, celebration){
+    for(let i = 0; i < names.length; i++){
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);
+    }
+    return thankYou;
+    debugger;
+}
+
+writeCards(names, "birthday");
+
+let countingNumber;
+
+function countDown(countingNumber){
+    while(countingNumber+1 > 0){
+        console.log(countingNumber--);
+    }
+}
+
+countDown(11);
